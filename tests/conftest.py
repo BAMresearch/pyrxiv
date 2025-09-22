a3f24f9a7f3e9fa1c66c5743fe911babd60c4311
jpizarro <jose.pizarro-blanco@bam.de>
2025-09-22 13:02:58 +0200

    Add testing for to_hdf5 and from_hdf5

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 
-from pyrxiv.datamodel import ArxivPaper
+from pyrxiv.datamodel import ArxivPaper, Author
 from pyrxiv.logger import log_storage
 
 if os.getenv("_PYTEST_RAISE", "0") != "0":
@@ -30,11 +30,15 @@ def generate_arxiv_paper(id: str = "1234.5678v1"):
         id=id,
         url=f"http://arxiv.org/abs/{id}",
         pdf_url=f"http://arxiv.org/pdf/{id}",
-        title="Test Title",
-        summary="A summary or abstract.",
-        authors=[],
-        comment="",
-        categories=[],
-        updated=datetime.datetime(2024, 4, 25, 0, 0, tzinfo=datetime.timezone.utc),
-        published=datetime.datetime(2024, 4, 25, 0, 0, tzinfo=datetime.timezone.utc),
+        updated=datetime.datetime(2025, 2, 21, 10, 0, 0),
+        published=datetime.datetime(2025, 2, 20, 12, 0, 0),
+        title="A test paper",
+        summary="This is a test summary",
+        authors=[Author(name="Alice"), Author(name="Bob")],
+        comment="Some comment",
+        n_pages=12,
+        n_figures=3,
+        categories=["cond-mat.str-el", "cond-mat.mtrl-sci"],
+        pdf_loader="pypdf",
+        text="This is the body of the paper.",
     )
